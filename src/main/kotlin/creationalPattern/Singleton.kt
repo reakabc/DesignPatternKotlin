9932f35afa793d67fa33dedca6fa51f677efd4e3
reakabc <nitishkumar020399@gmail.com>
2021-07-26 19:01:38 +0530

    Singleton class with parameters

--- src/main/kotlin/creationalPattern/Singleton.kt
+++ src/main/kotlin/creationalPattern/Singleton.kt
@@ -2,6 +2,7 @@ package creationalPattern
 
 /* Created by ReaK AbC on 26/07/2021 */
 
+/* Singleton class without any parameters */
 object Singleton{
 
     var createdBy : String? = null // ? -> createdBy can be null
@@ -16,9 +17,53 @@ object Singleton{
     }
 }
 
+/* Singleton class with parameters */
+
+class SingletonClass private constructor(name: String){
+
+    init {
+        println("Single class created with name $name")
+    }
+
+    companion object : SingletonHolder<SingletonClass, String>(::SingletonClass);
+
+}
+
+open class SingletonHolder<out T: Any, in A>(creator: (A)->T){
+
+    private var creator: ((A)->T)? = creator
+    @Volatile private var instance: T? = null;
+
+    fun getInstance(arg: A): T{
+        val checkInstance = instance
+        if (checkInstance != null){
+            return checkInstance;
+        }
+
+        return synchronized(this){
+            val checkInstanceAgain = instance
+            if (checkInstanceAgain != null){
+                checkInstanceAgain
+            }else{
+                val created = creator!!(arg)
+                instance = created
+                creator = null
+                created
+            }
+        }
+    }
+
+}
+
 fun main(args: Array<String>){
 
-    val name = Singleton.getCreatorName();
-    println(name?.length)
+    /*val name = Singleton.getCreatorName();
+    println(name?.length)*/
+
+    SingletonClass.getInstance("Nitish Kumar"); //only this will create the single object
+    SingletonClass.getInstance("Reak AbC")
+    SingletonClass.getInstance("Reak AbC")
+    SingletonClass.getInstance("Reak AbC")
+
 
 }
